cb017cdff7bccba9819ae7c4844fe7f55963c63b
harlan.brown@hyland.com <harlan.brown@hyland.com>
2023-11-22 13:54:22 -0600

    counting.records

--- example.py
+++ example.py
@@ -6,15 +6,17 @@ entries = []
 index = 0
 
 while True:
-    query_result = nuxeo.documents.query({"query":query, "pageSize":500, "currentPageIndex":index});
-    listOfEntries = query_result["entries"];
+    query_result = nuxeo.documents.query({"query":query, "pageSize":500, "currentPageIndex":index})
+    print("resultsCount: ",query_result["resultsCount"])
+    listOfEntries = query_result["entries"]
+    print("number of entries in this page: ", len(listOfEntries))
 
     for doc in listOfEntries:
-        entries.append(doc.properties)
-
+        entries.append(doc.uid)
+    print("number of uids appended to entries list: ", len(entries))
+    print("number of unique uids in entries list:", len(set(entries)))
     if not query_result['isNextPageAvailable']:
         break
 
     index += 1
-print(entries)
 
